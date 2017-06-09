5c5cf52f028f3dd5b101bd6a5907e17e0b85a53b
MaksKulykov <kulykov.maks@gmail.com>
2017-06-09 13:54:51 +0300

    added button scroll to top

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Renderer } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,11 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title: string = 'Каталог фильмов JSExpert';
+  title: string = 'Мой каталог фильмов';
 
   links = [
     { path: '/dashboard', icon: 'home', label: 'Главная'},
     { path: '/filmList', icon: 'theaters', label: 'Все фильмы'},
     { path: '/profile', icon: 'person', label: 'Профиль'}
   ];
+
+  constructor(private renderer: Renderer) {
+    this.renderer.listenGlobal('window', 'scroll', (evt) => {this.scrollFunction();} );
+  }
+
+  scrollFunction(): void {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      document.getElementById("btnTop").style.display = "block";
+    } else {
+      document.getElementById("btnTop").style.display = "none";
+    }
+  }
+
+  topFunction(): void {
+    document.body.scrollTop = 0; // For Chrome, Safari and Opera
+    document.documentElement.scrollTop = 0; // For IE and Firefox
+  }
 }
+
