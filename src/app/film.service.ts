2632b49df379623325c2b065a5a7982c205855fb
MaksKulikov <kulykov.maks@gmail.com>
2017-07-31 14:01:26 +0300

    refactored film-service

--- src/app/film.service.ts
+++ src/app/film.service.ts
@@ -3,37 +3,38 @@ import { Http, Response, URLSearchParams } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/observable/throw';
+import 'rxjs/add/operator/catch';
 
 @Injectable()
 export class FilmService {
-  private url: string = "http://www.omdbapi.com/";
-  private apiKey: string = '520bbe17';
+  private url = 'http://www.omdbapi.com/';
+  private apiKey = '520bbe17';
   constructor(private http: Http) { }
 
   private extractListData(res: Response): Array<any> {
-    let body = res.json();
+    const body = res.json();
     return body.Search || {};
   }
 
   private extractItemData(res: Response): Array<any> {
-    let body = res.json();
+    const body = res.json();
     return body || {};
   }
 
   getFilms(filmName: string, pageNumber: string): Observable<any> {
-    let params: URLSearchParams = new URLSearchParams();
+    const params: URLSearchParams = new URLSearchParams();
     params.set('apikey', this.apiKey);
     params.set('page', pageNumber || '1');
     params.set('s', filmName);
     return this.http.get(this.url, {search: params}).map(this.extractListData)
-      .catch((error: any)=> { return Observable.throw(error);});
+      .catch((error: any) => { return Observable.throw(error); });
   }
 
   getFilmById (filmId: string): Observable<any> {
-    let params: URLSearchParams = new URLSearchParams();
+    const params: URLSearchParams = new URLSearchParams();
     params.set('i', filmId);
     params.set('apikey', this.apiKey);
     return this.http.get(this.url, {search: params}).map(this.extractItemData)
-      .catch((error: any)=> { return Observable.throw(error);});
+      .catch((error: any) => { return Observable.throw(error); });
   }
 }
